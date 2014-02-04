0fa5aa43d874295ca863b21d631b0959ab3605d5
takuma03 <takuma@nkudou-no-iMac.local>
2014-02-04 21:43:47 +0900

    Happプロジェクト追加

---
+++ Happ/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Happ
+//
+//  Created by takuma on 2014/02/04.
+//  Copyright (c) 2014年 卓馬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Happ/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  Happ
+//
+//  Created by takuma on 2014/02/04.
+//  Copyright (c) 2014年 卓馬. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
