cba6156e9cfcbe84cf251c39a0fc345a7c427ba0
tkm03 <sebango3saitou@gmail.com>
2014-03-08 19:18:52 +0900

    Happ_csvUpload変更

--- Happ_csvUpload/Happ_csvUpload/Happ_csvUpload/ViewController.m
+++ Happ_csvUpload/Happ_csvUpload/Happ_csvUpload/ViewController.m
@@ -18,18 +18,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSString *samplePath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"csv"];
+    NSString *samplePath = [[NSBundle mainBundle] pathForResource:@"/Users/saitotakuma/Library/Application Support/iPhone Simulator/7.0.3/Applications/FE4C7B3C-5DEB-4D62-953A-DD0FF3A27C8F/Document/test" ofType:@"csv"];
 	NSData *sampleData = [NSData dataWithContentsOfFile:samplePath];
     
-    NSLog(@"%@",samplePath);
-    NSLog(@"%@",sampleData);
-    
 	//送信先URL
-	NSURL *url = [NSURL URLWithString:@"192.168.33.20"];
+	NSURL *url = [NSURL URLWithString:@"http://192.168.33.10/"];
     
-    NSLog(@"%@",url);
-    
-
 	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
 	[request setHTTPMethod:@"POST"];
     
@@ -46,7 +40,7 @@
 	NSString *fileName = [[samplePath componentsSeparatedByString:@"/"] lastObject];
     
 	//アップロードするファイルの種類
-	NSString *contentType = @"video/mp4";
+	NSString *contentType = @"csv";
     
 	NSMutableData *postBody = [NSMutableData data];
     
@@ -66,6 +60,40 @@
 	[NSURLConnection connectionWithRequest:request delegate:self];
 
  }
+
+
+
+
+
+
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    
+	NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+	if(httpResponse.statusCode == 200) {
+        
+		NSLog(@"Success ٩꒰๑ ´∇`๑꒱۶✧");
+        
+	} else {
+		
+		NSLog(@"Failed (´；ω；｀)");
+    }
+}
+              
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    
+    NSArray *jsonObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    NSLog(@"%@", jsonObject);
+}
+
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    
+    NSLog(@"%@", error);
+}
+              
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
