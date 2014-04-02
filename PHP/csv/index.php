3e1cc21652220677e9a7e769ebde046433ae0e5e
tkm03 <sebango3saitou@gmail.com>
2014-04-03 00:07:48 +0900

    csvフォルダ追加

---
+++ PHP/csv/index.php
@@ -0,0 +1,50 @@
+<?php
+require('dbconnect.php');
+//$mysql_id = array();
+$csv_id = array();
+//$n = 0;
+/*
+//データベースのデータを取得
+$recordSet = mysql_query('select * from task');
+while($data = mysql_fetch_assoc($recordSet)){
+        $mysql_id[$n] = $data['task_id'];
+        $n++;
+}
+*/
+
+$n = 0;
+//CSVのデータ取得
+if (($handle = fopen("test.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle)) !== FALSE) {
+            //$csv_id[$n] = $data[7];
+            if($data[7] == "'0'"){
+                        $sql = sprintf('insert into task values(' . $data[0] . ',' . $data[1] . ',' . $data[2] . ',' . $data[3] . ',' . $data[4] . ',' . $data[5] . ',' . $data[6] . ', 1 )' );
+                        echo   $sql . "<br />";
+                        mysql_query($sql) or die (mysql_error());
+                        //echo 'データを挿入しました';       
+                        //echo "<br />";
+            }elseif($data[7] == "'1'"){
+                        //MySQLからupdate_timeを取得する
+                        
+                        //CSVデータのupdate_time($data[6])と比較する
+                        //CSVデータ側が新しければCSV側のデータをMySQLへinsertする
+                        
+                        //MySQL側が新しければSQL文を作成する
+            }
+            $n++;
+    }
+    fclose($handle);
+}
+/*
+var_dump($csv_id);
+
+for($i = 0; $i < count($csv_id); $i++){
+        if($csv_id[$i] == "'0'"){ 
+                echo "<br />";
+                echo $i . "|" . $csv_id[$i] ."<br />";
+        }
+}
+*/
+
+
+?>
