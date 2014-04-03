4f07faa6fac836b7a8e729852b69bf450447a39a
takuma03 <takuma@nkudou-no-iMac.local>
2014-04-03 23:09:58 +0900

    MySQL変更

--- PHP/csv/index.php
+++ PHP/csv/index.php
@@ -30,11 +30,22 @@ if (($handle = fopen("test.csv", "r")) !== FALSE) {
                                 $mysql_update_time = $row['update_time'];
                         }
                         //CSVデータのupdate_time($data[6])と比較する
-                        echo $mysql_update_time . "<br />";         
-                        
+                        echo "MySQLのデータ：" . $mysql_update_time . "<br />";
+                        $csv_update_time = str_replace("'","",$data[6]);
+                        echo "CSVのデータ：" . $csv_update_time . "<br />";
+                        if(strtotime($csv_update_time) > strtotime($mysql_update_time)){
                         //CSVデータ側が新しければCSV側のデータをMySQLへinsertする
-                                
+                        		echo "iPhone側の方が新しいです<br />" ;
+                        		$sql = sprintf('update task set category =' . $data[1] . ', task_name =' . $data[2] . ', memo =' .$data[3] . ', start_time =' . $data[4] . ', stop_time =' . $data[5] . ',update_time =' . $data[6] . 'where task_id =' . $n );
+                        		mysql_query($sql) or die(mysql_error());
+                        		echo "データを挿入しました<br />";
                         //MySQL側が新しければSQL文を作成する
+                        }elseif(strtotime($csv_update_time) < strtotime($mysql_update_time)){
+                        		echo "MySQL側の方が新しいです<br />";
+                        }else{
+                        		echo "更新なし<br />";
+                        }
+                        
             }
             $n++;
     }
