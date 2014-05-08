98ed41a02e718659afdccbe33278d2c7509bbb24
takuma03 <takuma@nkudou-no-iMac.local>
2014-05-08 23:25:07 +0900

    modify Happ_webView

--- Happ_webView/Happ_webView/ViewController.m
+++ Happ_webView/Happ_webView/ViewController.m
@@ -41,6 +41,9 @@
     //-webkit-touch-callout: none;
     //-webkit-user-select: none;
     
+    
+    self.myWebView.delegate = self;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -62,6 +65,61 @@
         }
     }
     [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
+}
+
+- (BOOL)webView:(UIWebView *)aWebView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    
+    // もしNative連携用のURLスキームの場合には、処理する。
+    NSString *urlStr = request.URL.absoluteString;
+    if ([urlStr hasPrefix:@"api-yoheim://"]) {
+        
+    	// %エスケープされた内容を、デコードする。
+        urlStr = [urlStr stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        
+        // 不要な部分を削除する。
+        urlStr = [urlStr stringByReplacingOccurrencesOfString:@"api-yoheim://" withString:@""];
+        
+        // カスタムURLスキームで指定された指示を、正規表現を利用して特定します。
+        NSRegularExpression *regexp = [[NSRegularExpression alloc]
+                                       initWithPattern:@"^(.*?)\\{(.*?)\\}$"
+                                       options:0
+                                       error:nil];
+        
+        NSArray *matches = [regexp matchesInString:urlStr
+                                           options:0
+                                             range:NSMakeRange(0, urlStr.length)];
+        
+        for (NSTextCheckingResult *match in matches) {
+            // URLの内容から、メソッドとその引数を取得します。
+            NSString *method = [urlStr substringWithRange:[match rangeAtIndex:1]];
+            NSString *params = [urlStr substringWithRange:[match rangeAtIndex:2]];
+            #pragma clang diagnostic push
+            #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            // メソッドをコールします。
+            [self performSelector:NSSelectorFromString([NSString stringWithFormat:@"%@:", method])
+                       withObject:params];
+            #pragma clang diagnostic pop
+        }
+        
+        // カスタムスキームの場合には、リクエストを中止します。
+        return NO;
+    }
+    
+    // 上記以外は、リクエストが外部へ放出することを許可します。
+    return YES;
+}
+
+
+#pragma mark - Actions
+// JavaScriptから指定されて呼び出されるメソッド。
+-(void)someAlert:(NSString *)message {
+    UIAlertView *alert
+    = [[UIAlertView alloc] initWithTitle:@""
+                                 message:message
+                                delegate:nil
+                       cancelButtonTitle:@"OK"
+                       otherButtonTitles:nil];
+    [alert show];
 }
 
 @end
